251e684b58ffa53b0d993ea09ae94c75f62e11ea
wangyang <1749748955@qq.com>
2023-08-08 21:34:08 +0800

    update

---
+++ predict.sh
@@ -0,0 +1,8 @@
+diamond  blastx \
+    -q ./output/clean/test.clean \
+    -d /home/wangyang/workspace/gusphdproj-deeparg-ss-fbe063e24cf7/database/database/v2/features \
+    -k 1000 \
+    --id 80.0 \
+    --sensitive \
+    -e 1e-10 \
+    -a ./tmp2/test.clean.deeparg.align
